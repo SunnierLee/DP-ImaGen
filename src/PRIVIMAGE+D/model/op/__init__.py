43b2cfa67a40ba30858408fd4d4a3f34773c3b41
SunnierLee <121883928+SunnierLee@users.noreply.github.com>
2023-10-21 00:37:04 +0800

    Delete src/PRIVIMAGE+D directory

--- src/PRIVIMAGE+D/model/op/__init__.py
+++
@@ -1,4 +0,0 @@
-# from .fused_act import FusedLeakyReLU, fused_leaky_relu
-# from .upfirdn2d import upfirdn2d
-
-from .mine_fn import FusedLeakyReLU, fused_leaky_relu, upfirdn2d
--- src/PRIVIMAGE+D/pd.sh
+++
@@ -1,4 +0,0 @@
-for i in `seq 0 99`
-    do
-        python process_dataset.py --worker_id $i --num_workers 100 &
-    done
